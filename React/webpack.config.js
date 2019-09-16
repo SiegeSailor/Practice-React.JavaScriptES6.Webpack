d2f22bf574d8a652f71db91ecddeb804c924362b
SiegeSailor <siegesailor@gmail.com>
2019-09-16 20:14:38 +0800

    React :: In the beginning of 7-PropsAndState

--- React/webpack.config.js
+++ React/webpack.config.js
@@ -101,3 +101,28 @@ module.exports = {
         }]
     }
 };
+
+module.exports = {
+    entry: './7-PropsAndState.jsx',
+    output: {
+        filename: '7-PropsAndState.js',
+        path: path.resolve(__dirname, './'),
+
+    },
+    // 以下的 module 來自 webpack 官網的 babel loader 設定
+    // 7-PropsAndState
+    module: {
+        rules: [{
+            test: /\.jsx?$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    // 注意這裡有針對 react 的 preset
+                    presets: ['@babel/preset-env', '@babel/preset-react'],
+                    plugins: ["@babel/plugin-proposal-class-properties"]
+                }
+            }
+        }]
+    }
+};
