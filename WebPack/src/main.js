9fdbca19bcc901b16bc56c0719e10830cd4af4a5
siegesailor@gmail.com <siegesailor@gmail.com>
2019-12-11 21:45:28 +0800

    WebPack :: Till 9-BabelLoaderES6ToES5

--- WebPack/src/main.js
+++ WebPack/src/main.js
@@ -1,1 +1,15 @@
+import tool from "./tool.js";
+
 let test = 1;
+const test2 = 2;
+const test3 = 3;
+let a = "b";
+let b = "c";
+const plus = (value1, value2) => {
+    return value1 + value2
+}
+
+let sum = plus(1,2)
+console.log(sum)
+
+console.log(tool)
